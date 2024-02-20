0933818545e0530d4a39c9fad7fc9920490a1254
Matteo Larrode <matteo.larrode@gmail.com>
2024-02-20 15:15:35 +0000

    scraped & cleaned topological data raster

--- data-raw/wildfire_prediction.R
+++ data-raw/wildfire_prediction.R
@@ -1,10 +1,11 @@
 # ---- WILDFIRE PREDICTION ----
-
-# ---- Setup ----
+# ---- SETUP ----
 library("sf")
 library("tmap")
+library("raster")
 library("tidyverse")
 library("geographr")
+library("httr2")
 
 # MSOA (or equivalent) boundaries for all UK nations
 msoa <- geographr::boundaries_msoa21
@@ -21,7 +22,10 @@ msoa_uk <- rbind(msoa, iz, sdz) |>
   st_make_valid() |> 
   mutate(area_km2 = as.numeric(st_area(geometry) / 1e6))
 
-# ---- Fire point data ----
+countries_uk_wgs84 <- geographr::boundaries_countries20 |> 
+  st_transform(crs = "+proj=longlat +datum=WGS84")
+
+# ---- FIRE POINT DATA ----
 fire_all <- 
   read_sf("inst/extdata/DL_FIRE_M-C61_427984/fire_archive_M-C61_427984.shp") |> 
   select(LATITUDE, LONGITUDE, ACQ_DATE, geometry) |> 
@@ -44,19 +48,10 @@ fires_summary <- fires_spring_msoa |>
   group_by(msoa21_code) |> 
   summarize(total_fires = n())
 
-
-
-
-
-
-
-
-
-# ---- Visualisations ----
+# Visualisations
 tm_shape(fire_modis_all) +
   tm_dots(col = "red", size = 0.05, shape = 16, border.col = "black", title = "Wildfires") +
   tm_layout(frame = FALSE)
-
 
 ggplot(fire_modis_all_yearly, aes(x = year, y = num_wildfires)) +
   geom_line() +
@@ -64,3 +59,60 @@ ggplot(fire_modis_all_yearly, aes(x = year, y = num_wildfires)) +
   ggtitle("Number of Wildfires Over Years") +
   scale_x_continuous(breaks = as.integer(fire_modis_all_yearly$year))+
   theme_minimal()
+
+# ---- INDEPENDENT VARIABLES ----
+# ---- Topography ----
+# Source: https://www.worldclim.org/data/worldclim21.html
+topography_url <- "https://biogeo.ucdavis.edu/data/worldclim/v2.1/base/wc2.1_2.5m_elev.zip"
+
+download <- tempfile()
+
+request(topography_url) |>
+  req_perform(download)
+
+elevation_raw <- raster(unzip(download))
+elevation_uk <- crop(elevation_raw, countries_uk_wgs84)
+elevation_uk_mask <- mask(elevation_uk, countries_uk_wgs84)
+
+# Reduce number of missing values
+sum(is.na(elevation_uk[]))
+filled_elevation <- focal(elevation_uk, w=matrix(1, nrow=3, ncol=3), fun=mean, na.rm=TRUE)
+elevation_uk[is.na(elevation_uk)] <- filled_elevation[is.na(elevation_uk)]
+
+# Calculate slope & aspect using the terrain function
+slope_raster <- terrain(elevation_uk, opt = "slope")
+slope_raster_mask <- mask(slope_raster,countries_uk_wgs84)
+
+aspect_raster <- terrain(elevation_uk, opt = "aspect")
+aspect_raster_mask <- mask(aspect_raster,countries_uk_wgs84)
+
+# Visualisation
+tm_shape(elevation_uk_mask) +
+  tm_raster(style = "cont", 
+            title = "Elevation", 
+            palette= "-Spectral", 
+            midpoint = NA) +
+  tm_layout(frame = FALSE, 
+            legend.position = c("right", "top"), 
+            title.position = c("left", "bottom"))
+
+tm_shape(slope_raster_mask) +
+  tm_raster(style = "cont", 
+            title = "Slope", 
+            palette = "-Spectral", 
+            midpoint = NA) +
+  tm_layout(frame = FALSE, 
+            legend.position = c("right", "top"), 
+            title.position = c("left", "bottom"))
+
+tm_shape(aspect_raster_mask) +
+  tm_raster(style = "cont", 
+            title = "Slope", 
+            palette = "-Spectral", 
+            midpoint = NA)+
+  tm_layout(frame = FALSE, 
+            legend.position = c("right", "top"), 
+            title.position = c("left", "bottom"))
+
+# ---- Climate variables ----
+
